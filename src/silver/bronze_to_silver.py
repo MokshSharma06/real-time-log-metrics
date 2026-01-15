d3102d46a042417d007a44de16f9b6751f4418df
moksh <mokshsharma06@gmail.com>
2026-01-15 21:59:49 +0530

    flagged bad/late/clean data and wrote them to specific sinks

--- src/silver/bronze_to_silver.py
+++ src/silver/bronze_to_silver.py
@@ -1,4 +1,4 @@
-from pyspark.sql.functions import from_json, col, to_timestamp
+from pyspark.sql.functions import from_json, col, to_timestamp,expr
 from src.utils import get_spark_session
 from src.schema import LOG_SCHEMA
 
@@ -15,29 +15,76 @@ parsed_df = bronze_df.withColumn(
 )
 
 silver_df = parsed_df.select(
-    col("data.event_id").alias("event_id"),
-    col("data.service").alias("service_name"),
+    col("data.event_id").alias("event_id"),  col("data.service").alias("service_name"),
     col("data.status_code").alias("status_code"),
     to_timestamp(col("data.event_time")).alias("event_time"),
     col("ingestion_time"),
     col("kafka_partition"),
     col("kafka_offset")
 )
-silver_df = silver_df.filter(col("event_time").isNotNull())
+is_valid_expr = (
+    col("event_id").isNotNull() & 
+    col("event_time").isNotNull() & 
+    (col("status_code") >= 100) & (col("status_code") <= 500)
+)
 
-query = silver_df.writeStream \
-    .format("delta") \
-    .outputMode("append") \
+is_late_expr = expr("event_time < ingestion_time - INTERVAL 10 MINUTES")
+processed_stream = (
+    silver_df
+    .withColumn("is_valid", is_valid_expr)
+    .withColumn("is_late", is_late_expr)
+    .withWatermark("event_time", "24 hours") 
+    .dropDuplicates(["event_id", "event_time"])
+)
+def multi_sink_writer(batch_df, batch_id):
+    batch_df.persist()
+    internal_flags = ["is_valid", "is_late"]
+
+    # CLEAN DATA
+    (batch_df
+     .filter((col("is_valid") == True) & (col("is_late") == False))
+     .drop(*internal_flags)
+     .write.format("delta")
+     .mode("append")
+     .save("data/silver/clean"))
+
+    # BAD DATA
+    (batch_df
+     .filter(col("is_valid") == False)
+     .drop(*internal_flags)
+     .write.format("delta")
+     .mode("append")
+     .save("data/silver/bad"))
+
+    # LATE DATA 
+    (batch_df
+     .filter((col("is_valid") == True) & (col("is_late") == True))
+     .drop(*internal_flags)
+     .write.format("delta")
+     .mode("append")
+     .save("data/silver/late"))
+
+    batch_df.unpersist()
+
+
+query = processed_stream.writeStream \
+    .foreachBatch(multi_sink_writer) \
     .trigger(availableNow=True) \
     .option("checkpointLocation", "checkpoints/silver/logs") \
-    .start("data/silver/logs")
+    .start()
 
 query.awaitTermination()
 
 
 # silver_df =spark.read \
 #         .format('delta') \
-#         .load('data/silver/logs')
+#         .load('data/silver/late')
 
-# silver_df.show()
-e
+# from pyspark.sql import functions as F
+
+# target_id = "7b7511bf-9deb-451b-be8f-51b93d153e74"
+# filtered_df = silver_df.filter(F.col("event_id") == target_id)
+
+# filtered_df.show()
+
+# silver_df.show(10000)

6c3f216d9c4e43f9ab023dd480574e653b45acd7
moksh <mokshsharma06@gmail.com>
2026-01-16 21:56:17 +0530

    configured spark with 3.1.0 delta-support

--- src/utils.py
+++ src/utils.py
@@ -20,11 +20,12 @@ def get_spark_session():
     from pyspark.sql import SparkSession
 
     return (
-        SparkSession.builder
-            .appName("real-time-log-metrics") \
-            .config("spark.sql.session.timeZone","UTC") \
-            .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.0.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
-    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-    .getOrCreate()
-    )
+    SparkSession.builder
+        .appName("real-time-log-metrics")
+        .config("spark.sql.session.timeZone", "UTC")
+        # Updated delta-spark to 3.1.0 to match Spark 3.5.0
+        .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.1.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0")
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+        .getOrCreate()
+)

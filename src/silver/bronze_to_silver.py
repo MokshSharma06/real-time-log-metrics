c80ef3fb9c46120a0fdaf36c2a1cff77bc741ef3
moksh <mokshsharma06@gmail.com>
2026-01-18 21:55:25 +0530

    classified data with new cols record status and rejection reason for auditability

--- src/silver/bronze_to_silver.py
+++ src/silver/bronze_to_silver.py
@@ -2,6 +2,9 @@ from pyspark.sql.functions import from_json, col, to_timestamp,expr
 from src.utils import get_spark_session
 from src.schema import LOG_SCHEMA
 from delta.tables import DeltaTable
+from src.transformations import classify_data
+from pyspark.sql.functions import col
+
 
 spark = get_spark_session()
 
@@ -32,11 +35,13 @@ is_valid_expr = (
 is_late_expr = expr("event_time < ingestion_time - INTERVAL 10 MINUTES")
 processed_stream = (
     silver_df
-    .withColumn("is_valid", is_valid_expr)
+    .withColumn("schema_valid", is_valid_expr)
     .withColumn("is_late", is_late_expr)
     .withWatermark("event_time", "2 hours")
     .dropDuplicates(["event_id", "event_time"])
 )
+classified_stream = classify_data(processed_stream)
+
 # paths for the sinks
 clean_path = "data/silver/clean"
 bad_path = "data/silver/bad"
@@ -44,9 +49,11 @@ late_path = "data/silver/late"
 
 def multi_sink_writer(batch_df, batch_id):
     batch_df.persist()
-    internal_flags = ["is_valid", "is_late"]
+    internal_flags = ["schema_valid", "is_late"]
 
-    clean_data = batch_df.filter((col("is_valid") == True) & (col("is_late") == False)).drop(*internal_flags)
+    clean_data = batch_df.filter(col("record_status") == "CLEAN").drop(*internal_flags)
+    bad_data   = batch_df.filter(col("record_status") == "BAD").drop(*internal_flags)
+    late_data  = batch_df.filter(col("record_status") == "LATE").drop(*internal_flags)
     
 
     if not clean_data.isEmpty():
@@ -57,35 +64,45 @@ def multi_sink_writer(batch_df, batch_id):
              .whenNotMatchedInsertAll()
              .execute())
         else:
-            clean_data.write.format("delta").mode("append").save(clean_path)
+            clean_data.write.format("delta").mode("append").option("mergeSchema","true").save(clean_path)
 
 
     # BAD DATA
-    (batch_df
-     .filter(col("is_valid") == False)
-     .drop(*internal_flags)
-     .write.format("delta")
-     .mode("append")
-     .save("data/silver/bad"))
+    bad_data.write.format("delta") \
+    .mode("append") \
+    .option("mergeSchema","true") \
+    .save(bad_path)
 
     # LATE DATA 
-    (batch_df
-     .filter((col("is_valid") == True) & (col("is_late") == True))
-     .drop(*internal_flags)
-     .write.format("delta")
-     .mode("append")
-     .save("data/silver/late"))
+    late_data.write.format("delta") \
+    .mode("append") \
+    .option("mergeSchema","true") \
+    .save(late_path)
 
     batch_df.unpersist()
+
+query = (
+    classified_stream
+    .writeStream
+    .queryName("silver_layer")
+    .foreachBatch(multi_sink_writer)
+    .option("checkpointLocation", "checkpoints/silver")
+    .outputMode("update")
+    .start()
+)
+
+query.awaitTermination()
+
 
 
 silver_df =spark.read \
         .format('delta') \
-        .load('data/silver/clean')
+        .load('data/silver/late')
 
 # from pyspark.sql import functions as F
 # from datetime import date
 
-# target_id = "421bb00f-3e3f-4ee9-870d-234c0a160fed"
-# filtered_df = silver_df.filter(F.to_date(F.col("ingestion_time")) == F.current_date())
-# filtered_df.show()
+# # target_id = "421bb00f-3e3f-4ee9-870d-234c0a160fed"
+# # filtered_df=silver_df.filter(F.col("event_id")==target_id)
+# # filtered_df = silver_df.filter(F.to_date(F.col("ingestion_time")) == F.current_date())
+# silver_df.show()

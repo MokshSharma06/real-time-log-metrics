49f25710e724f92bdcc63e845e284eca3284a51f
MokshSharma06 <mokshsharma06@gmail.com>
2026-01-27 15:11:39 +0000

    modified config loader to parse config based on env selection

--- src/utils.py
+++ src/utils.py
@@ -7,40 +7,24 @@ from pyspark.sql import SparkSession
 
 class AppConfig:
     def __init__(self, config_path="conf/config.yaml"):
-        with open(config_path, 'r') as file:
+
+        with open(config_path, "r") as file:
             self._config = yaml.safe_load(file)
-        
-        self.env = self._config['environment']
-        self.paths, self.checkpoints = self._build_paths()
-        self.kafka = self._config['kafka']
-        self.streaming = self._config['streaming']
-        self.dedupe = self._config['deduplication']
 
-    def _build_paths(self):
-        if self.env == "azure":
-            acc = self._config['azure']['account_name']
-            data_cont = self._config['azure']['data_container']
-            check_cont = self._config['azure']['checkpoint_container']
-            prefix = self._config['azure']['sub_prefix']
-            
-            data_base = f"abfss://{data_cont}@{acc}.dfs.core.windows.net/{prefix}".strip("/")
-            meta_base = f"abfss://{check_cont}@{acc}.dfs.core.windows.net/{prefix}".strip("/")
-        else:
-            data_base = os.path.abspath(self._config['local']['data_base'])
-            meta_base = os.path.abspath(self._config['local']['checkpoint_base'])
+        env = self._config["environment"]
+
+        # env-resolved configs
+        self.paths = self._config["paths"][env]
+        self.checkpoints = self._config["checkpoints"][env]
+        self.kafka = self._config["kafka"][env]
+
+        # env-agnostic configs
+        self.streaming = self._config["streaming"]
+        self.dedupe = self._config["deduplication"]
+
+cfg=AppConfig(config_path="/Workspace/Repos/mokhsharma@niveditasawatsh2004gmail.onmicrosoft.com/real-time-log-metrics/conf/config.yaml")
 
 
-        paths = {k: f"{data_base}/{v}" for k, v in self._config['paths'].items()}
-        
-        checkpoints = {
-            "bronze": f"{meta_base}/bronze",
-            "silver": f"{meta_base}/silver",
-            "gold": f"{meta_base}/gold"
-        }
-        return paths, checkpoints
-
-cfg = AppConfig()
-    
 
 from pyspark.sql import SparkSession
 
